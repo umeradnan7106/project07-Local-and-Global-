b0f43f993aae10ddea03e11173c0f91d87b0a37d
umeradnan7106 <120960218+umeradnan7106@users.noreply.github.com>
2024-07-10 04:00:08 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//  Global Variable 
+// global variable is using every where
+var age = 18;
+if (true) {
+    console.log(age);
+}
+// Local variable
+// in local variable we use variable only in block ("{}")
+if (true) {
+    var firstName = "Umer";
+    console.log(firstName);
+}
+// console.log(firstName);    // don't use 
---
+++ main.ts
@@ -0,0 +1,18 @@
+//  Global Variable 
+
+// global variable is using every where
+let age = 18;
+if(true) {
+    console.log(age);
+}
+
+// Local variable
+
+// in local variable we use variable only in block ("{}")
+
+if (true){
+    let firstName = "Umer";
+    console.log(firstName);
+}
+// console.log(firstName);    // don't use after block
+
